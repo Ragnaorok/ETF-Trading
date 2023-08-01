0c4be73ce6835f278763acb5dc03492720b00885
Tom1581 <123705814+Tom1581@users.noreply.github.com>
2023-07-31 20:19:56 -0700

    Update Data_Retriever.py API

--- Data_Retriever.py
+++ Data_Retriever.py
@@ -1,24 +1,23 @@
-import yfinance as yf
-import json
+import time
 import datetime
+import pandas as pd
+import json
 
-# Get the current date
-current = datetime.date.today()
-
-# Define the tickers based on the ETFs you want to track
-tickers = ["FNGU", "FNGD"]
-# Define an empty dictionary to hold all data
+tickers = ['FNGU', 'FNGD']
 all_data = {}
 
-# Download the data for each ticker and store it in the dictionary
 for ticker in tickers:
-    data = yf.download(ticker, start='2020-01-01', end=current.isoformat())
-    all_data[ticker] = json.loads(data.reset_index().to_json(date_format='iso', orient='records'))
+    period1 = int(time.mktime(datetime.datetime(2020, 12, 1, 23, 59).timetuple()))
+    period2 = int(time.mktime(datetime.datetime(2020, 12, 31, 23, 59).timetuple()))
+    interval = '1d' # 1d, 1m
+    API_endpoint = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+    df = pd.read_csv(API_endpoint)
+    all_data[ticker] = json.loads(df.to_json(date_format='iso', orient='records'))
 
 # Convert the dictionary to JSON format
-
 FNGU_json = json.dumps(all_data["FNGU"])
 FNGD_json = json.dumps(all_data["FNGD"])
+
 # Save the JSON data to a file
 with open('FNGU.json', 'w') as file:
     file.write(FNGU_json)
