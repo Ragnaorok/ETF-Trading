dccb0d41fb58b602766b134f3ea8e221b3e9c82a
Tom1581 <123705814+Tom1581@users.noreply.github.com>
2023-08-01 11:57:35 -0700

    Update Data_Retriever.py

--- Data_Retriever.py
+++ Data_Retriever.py
@@ -8,7 +8,7 @@ all_data = {}
 
 for ticker in tickers:
     period1 = int(time.mktime(datetime.datetime(2020, 1, 1, 23, 59).timetuple()))
-    period2 = int(time.mktime(datetime.datetime.now().timetuple())) - 86400 # subtract one day to get yesterday's date
+    period2 = int(time.mktime(datetime.datetime.now().timetuple()))  # subtract one day to get yesterday's date
     interval = '1d' # 1d, 1m
     API_endpoint = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
     df = pd.read_csv(API_endpoint)
@@ -23,3 +23,4 @@ with open('FNGU.json', 'w') as file:
     file.write(FNGU_json)
 with open('FNGD.json', 'w') as file:
     file.write(FNGD_json)
+
